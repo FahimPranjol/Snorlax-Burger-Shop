3e719fd312b9a73fb43d56d117e609fd57ea3dba
rajanjaiswal <31754949+rajanjaiswal@users.noreply.github.com>
2018-04-06 01:21:25 -0500

    Update database.py

--- Reservation/src/common/database.py
+++ Reservation/src/common/database.py
@@ -2,20 +2,20 @@ import pymongo
 
 __author__ = 'jslvtr'
 
-
+//creating the class 
 class Database(object):
     URI = "mongodb://127.0.0.1:27017"
     DATABASE = None
-
+//connecting to the database
     @staticmethod
     def initialize():
         client = pymongo.MongoClient(Database.URI)
         Database.DATABASE = client['fullstack']
-
+//inserting the data from database
     @staticmethod
     def insert(collection, data):
         Database.DATABASE[collection].insert(data)
-
+//collecting the data from database
     @staticmethod
     def find(collection, query):
         return Database.DATABASE[collection].find(query)
