1b1d6fe196a0405d5605ed0da696131e299bef4c
FahimPranjol <pranjolfahim@gmail.com>
2018-04-06 23:20:51 -0500

    Add files via upload

--- Reservation/src/models/customer.py
+++ Reservation/src/models/customer.py
@@ -5,24 +5,27 @@ from flask import session
 from src.models.menu import Menu
 
 
+#creating customer object
 class Customer(object):
     def __init__(self, email, password, _id=None):
         self.email = email
         self.password = password
         self._id = uuid.uuid4().hex if _id is None else _id
 
+    #getting customer by email
     @classmethod
     def get_by_email(cls, email):
         data = Database.find_one("customers", {"email": email})
         if data is not None:
             return cls(**data)
 
+    #getting customer by id if ID is assigned
     @classmethod
     def get_by_id(cls, _id):
         data = Database.find_one("customers", {"_id": _id})
         if data is not None:
             return cls(**data)
-
+    #validating the user's email id and password
     @staticmethod
     def login_valid(email, password):
         # Check whether a user's email matches the password they sent us
@@ -32,6 +35,7 @@ class Customer(object):
             return customer.password == password
         return False
 
+    #registering the customer by asking for their information
     @classmethod
     def register(cls, email, password):
         customer = cls.get_by_email(email)
@@ -47,24 +51,26 @@ class Customer(object):
 
 
 
-
+    #checking if login is succssfull
     @staticmethod
     def login(user_email):
         # login_valid has already been called
         session['email'] = user_email
 
+    #logging out
     @staticmethod
     def logout():
         session['email'] = None
 
 
-
+    #inserting data into the database as json
     def json(self):
         return {
             "email": self.email,
             "password": self.password
         }
 
+    #saving information to the database
     def save_to_mongo(self):
         Database.insert("customers", self.json())
 
